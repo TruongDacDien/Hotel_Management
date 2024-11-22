b2d48e11096383b3c7cc9ae0d7c5211fe4ea2373
TruongDacDien <22520248@gm.uit.edu.vn>
2024-11-22 17:52:32 +0700

    Upload new codebase

---
+++ BUS/PhongBUS.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DTO;
+using DAL;
+using DAL.Data;
+
+namespace BUS
+{
+    public class PhongBUS
+    {
+        private static PhongBUS Instance;
+
+        private PhongBUS()
+        {
+
+        }
+
+        public static PhongBUS GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new PhongBUS();
+            }
+            return Instance;
+        }
+        
+
+        public List<Phong_Custom> getDataPhongCustomTheoNgay(DateTime? ngayChon)
+        {
+            return PhongDAL.GetInstance().getDataPhongTheoNgay(ngayChon);
+        }
+
+        public List<PhongTrong> getPhongTrong(DateTime? ngayBD, DateTime? ngayKT)
+        {
+            return PhongDAL.GetInstance().getPhongTrong(ngayBD,ngayKT);
+        }
+        public decimal? tinhTienPhong(Phong_Custom phong)
+        {
+            decimal? tienPhong;
+            tienPhong = PhongDAL.GetInstance().layGiaTienTheoMaPhong(phong);
+            if(phong.IsDay== true)
+            {
+                return phong.SoNgayO * tienPhong;
+            }
+            else
+            {
+                return phong.SoGio * tienPhong;
+            }
+        }
+        public decimal layTienPhongTheoSoPhong(Phong_Custom phong)
+        {
+            return PhongDAL.GetInstance().layGiaTienTheoMaPhong(phong);
+        }
+
+        public bool suaTinhTrangDonDep(string maPhong, string text, out string error)
+        {
+            return PhongDAL.GetInstance().suaTinhTrangPhong(maPhong, text, out error);
+        }
+    }
+}
---
+++ DAL/Data/PhongDAL.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DTO;
+using System.Data.Entity.SqlServer;
+
+namespace DAL.Data
+{
+    public class PhongDAL
+    {
+        private static PhongDAL Instance;
+
+        private PhongDAL()
+        {
+
+        }
+
+        public static PhongDAL GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new PhongDAL();
+            }
+            return Instance;
+        }
+
+      
+        public List<Phong_Custom> getDataPhongTheoNgay(DateTime? ngayChon)
+        {
+            
+            List<Phong_Custom> ls = new List<Phong_Custom>();
+
+            using (QLKhachSanEntities db = new QLKhachSanEntities())
+            {
+                //lấy ra những phòng đang thuê hoặc đã được đặt
+                var lsCTPT = db.CT_PhieuThue.Where(p => p.NgayBD <= ngayChon && p.NgayKT >= ngayChon && p.TinhTrangThue.CompareTo("Đã thanh toán") != 0 ).AsEnumerable();
+                // join với bảng phòng để lấy ra danh sách phòng nếu phòng nào mà CTPT == null thì phòng đó có nghĩa là trống và ngược lại thì là đã được thuê hoặc đang thuê kết quả ta được 1 list Phòng Custom để hiển thị lên UC_Phong
+                ls = (from p in db.Phongs
+                      join ct in lsCTPT on p.SoPhong equals ct.SoPhong into t
+                      from ct in t.DefaultIfEmpty()
+                      select new Phong_Custom()
+                      {
+                          MaCTPT = ct.MaCTPT,
+                          TenKH = (ct.PhieuThue.KhachHang.TenKH == null) ? "" : ct.PhieuThue.KhachHang.TenKH,
+                          MaPhong = p.SoPhong,
+                          DonDep = p.TinhTrang,
+                          TinhTrang = (ct.TinhTrangThue == null) ? "Phòng trống" : ct.TinhTrangThue,
+                          LoaiPhong = p.LoaiPhong.TenLoaiPhong,
+                          NgayDen = ct.NgayBD,
+                          SoNgayO = (ct.NgayBD == null) ? 0 : (int)SqlFunctions.DateDiff("day", ct.NgayBD, ct.NgayKT) + 1,
+                          SoGio = (ct.NgayBD == null) ? 0 : (int)SqlFunctions.DateDiff("hour", ct.NgayBD, ct.NgayKT)  ,
+                          NgayDi = ct.NgayKT,
+                          SoNguoi = (ct.SoNguoiO == null) ? 0 : ct.SoNguoiO
+                      }).ToList();
+
+            }
+            return ls;
+        }
+
+        public bool suaTinhTrangPhong(string maPhong, string text, out string error)
+        {
+            error = string.Empty;
+            try
+            {
+                using (QLKhachSanEntities db = new QLKhachSanEntities())
+                {
+                    Phong ph = db.Phongs.FirstOrDefault(p => p.SoPhong.Equals(maPhong));
+                    if(ph == null)
+                    {
+                        error = "Không tồn tại phòng có số phòng: " + maPhong;
+                        return false;
+                    }
+                    else
+                    {
+                        ph.TinhTrang = text;
+                        db.SaveChanges();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return false;
+            }
+        }
+
+        //lấy ra giá tiền
+        public decimal layGiaTienTheoMaPhong(Phong_Custom phong)
+        {
+            using (QLKhachSanEntities db = new QLKhachSanEntities())
+            {
+                if(phong.IsDay == true)
+                    return db.Phongs.FirstOrDefault(p => p.SoPhong.Equals(phong.MaPhong)).LoaiPhong.GiaNgay;
+                else
+                    return db.Phongs.FirstOrDefault(p => p.SoPhong.Equals(phong.MaPhong)).LoaiPhong.GiaGio;
+            }
+        }
+
+        public List<PhongTrong> getPhongTrong(DateTime? ngayBD , DateTime? ngayKT)
+        {
+            List<PhongTrong> lsPTrong = new List<PhongTrong>();
+            using (QLKhachSanEntities db = new QLKhachSanEntities())
+            {
+                //lấy ra phòng đã được thuê trong khoảng thời gian chọn được truyền vào
+                var lsCTPT = db.CT_PhieuThue.Where(p => (   (p.NgayBD <= ngayBD && p.NgayKT >= ngayKT) || //Trường hợp 1: Ngày được chọn nằm giữ khoảng ngày bắt đầu và ngày kết thúc của phòng đó.
+                                                            (p.NgayBD >= ngayBD && p.NgayBD <= ngayKT) || //Trường hợp 2: Ngày được chọn dính vào phần đầu của ngày của phòng.
+                                                            (p.NgayKT >= ngayBD && p.NgayKT <= ngayKT) || //Trường hợp 3: Ngày được chọn dính vào phần sau của ngày của phòng.
+                                                            (p.NgayBD >= ngayBD && p.NgayKT <= ngayKT) ) && //Trườn hợp 4: Ngày được chọn bao phủ ngày của phòng .
+                                                             p.TinhTrangThue.CompareTo("Đã thanh toán") != 0
+                                                            ).AsEnumerable();
+                //sau khi đã lấy được những phòng đang thuê thì ta ngoại trừ những phòng đó ra ta được những phòng được phép thuê 
+                lsPTrong = (from p in db.Phongs
+                            where !( from ct in lsCTPT
+                                     select ct.SoPhong).Contains(p.SoPhong )
+                            select new PhongTrong()
+                            { SoPhong = p.SoPhong,
+                              TenLoaiPhong= p.LoaiPhong.TenLoaiPhong
+                            }).ToList();
+            }
+            return lsPTrong;
+        }
+
+
+    }
+}
