af0ad772ebb8d24792f2ab1e2ec25c773edaa7c7
TruongDacDien <22520248@gm.uit.edu.vn>
2024-12-07 16:18:59 +0700

    Fix DTO

--- DAL/DTO/Phong.cs
+++ DAL/DTO/Phong.cs
@@ -10,13 +10,13 @@ namespace DAL.DTO
 	{
 		private string soPhong;
 		private int maLoaiPhong;
-		private string tinhTrang;
+		private string donDep;
 		private string loaiPhong;
 		private bool isDeleted;
 
 		public string SoPhong { get => soPhong; set => soPhong = value; }
 		public int MaLoaiPhong { get => maLoaiPhong; set => maLoaiPhong = value; }
-		public string TinhTrang { get => tinhTrang; set => tinhTrang = value; }
+		public string DonDep { get => donDep; set => donDep = value; }
 		public string LoaiPhong { get => loaiPhong; set => loaiPhong = value; }
 		public bool IsDeleted { get => isDeleted; set => isDeleted = value; }
 	}
