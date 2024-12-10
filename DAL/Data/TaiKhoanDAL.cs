31942a6aae36c7204ff1c93dfb719311b78b9c73
TruongDacDien <22520248@gm.uit.edu.vn>
2024-12-10 16:51:06 +0700

    Quản lý tài khoản

--- DAL/Data/TaiKhoanDAL.cs
+++ DAL/Data/TaiKhoanDAL.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.IO;
 using MySql.Data.MySqlClient;
 using System.Drawing;
+using System.Collections.Generic;
 
 namespace DAL.Data
 {
@@ -120,5 +121,209 @@ namespace DAL.Data
 				return false;
 			}
 		}
+
+		// Lấy danh sách tất cả tài khoản
+		public List<TaiKhoan> getDataTaiKhoan()
+		{
+			List<TaiKhoan> danhSachTaiKhoan = new List<TaiKhoan>();
+			string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+			try
+			{
+				using (MySqlConnection conn = new MySqlConnection(connectionString))
+				{
+					string query = @"SELECT tk.Username, tk.Password, tk.MaNV, tk.CapDoQuyen, tk.Avatar, tk.Disabled,
+                            nv.HoTen, nv.ChucVu, nv.SDT, nv.DiaChi, nv.CCCD, nv.NTNS, nv.GioiTinh, nv.Luong
+                            FROM TaiKhoan tk
+                            LEFT JOIN NhanVien nv ON tk.MaNV = nv.MaNV AND tk.Disabled = 0";
+
+					MySqlCommand cmd = new MySqlCommand(query, conn);
+					conn.Open();
+
+					using (MySqlDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							TaiKhoan taiKhoan = new TaiKhoan
+							{
+								Username = reader["Username"].ToString(),
+								Password = reader["Password"].ToString(),
+								MaNV = reader.IsDBNull(reader.GetOrdinal("MaNV")) ? 0 : reader.GetInt32(reader.GetOrdinal("MaNV")),
+								CapDoQuyen = reader.IsDBNull(reader.GetOrdinal("CapDoQuyen")) ? 0 : reader.GetInt32(reader.GetOrdinal("CapDoQuyen")),
+								Avatar = reader["avatar"] as byte[],
+								Disabled = reader.IsDBNull(reader.GetOrdinal("Disabled")) ? false : reader.GetBoolean(reader.GetOrdinal("Disabled")),
+								NhanVien = new NhanVien
+								{
+									MaNV = reader.IsDBNull(reader.GetOrdinal("MaNV")) ? 0 : reader.GetInt32(reader.GetOrdinal("maNV")),
+									HoTen = reader["HoTen"]?.ToString(),
+									ChucVu = reader["ChucVu"]?.ToString(),
+									SDT = reader["SDT"]?.ToString(),
+									DiaChi = reader["DiaChi"]?.ToString(),
+									CCCD = reader["CCCD"]?.ToString(),
+									NTNS = reader.IsDBNull(reader.GetOrdinal("NTNS")) ? DateTime.MinValue : reader.GetDateTime(reader.GetOrdinal("NTNS")),
+									GioiTinh = reader["GioiTinh"]?.ToString(),
+									Luong = reader.IsDBNull(reader.GetOrdinal("Luong")) ? 0 : reader.GetDecimal(reader.GetOrdinal("Luong"))
+								}
+							};
+
+							danhSachTaiKhoan.Add(taiKhoan);
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Lỗi: " + ex.Message);
+			}
+
+			return danhSachTaiKhoan;
+		}
+
+		public bool themTaiKhoan(TaiKhoan taiKhoan)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+			try
+			{
+				using (MySqlConnection conn = new MySqlConnection(connectionString))
+				{
+					string query = @"INSERT INTO TaiKhoan (Username, Password, MaNV, CapDoQuyen, Avatar, Disabled)
+                             VALUES (@Username, @Password, @MaNV, @CapDoQuyen, @Avatar, @Disabled)";
+					MySqlCommand cmd = new MySqlCommand(query, conn);
+					cmd.Parameters.AddWithValue("@Username", taiKhoan.Username);
+					cmd.Parameters.AddWithValue("@Password", taiKhoan.Password);
+					cmd.Parameters.AddWithValue("@MaNV", taiKhoan.MaNV);
+					cmd.Parameters.AddWithValue("@CapDoQuyen", taiKhoan.CapDoQuyen);
+					cmd.Parameters.AddWithValue("@Avatar", taiKhoan.Avatar ?? (object)DBNull.Value);
+					cmd.Parameters.AddWithValue("@Disabled", taiKhoan.Disabled);
+
+					conn.Open();
+					cmd.ExecuteNonQuery();
+					return true;
+				}
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public bool capNhatTaiKhoan(TaiKhoan taiKhoan)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+			try
+			{
+				using (MySqlConnection conn = new MySqlConnection(connectionString))
+				{
+					string query = @"UPDATE TaiKhoan
+                             SET Password = @Password,
+                                 MaNV = @MaNV,
+                                 CapDoQuyen = @CapDoQuyen,
+                                 Avatar = @Avatar,
+                                 Disabled = @Disabled
+                             WHERE username = @Username";
+					MySqlCommand cmd = new MySqlCommand(query, conn);
+					cmd.Parameters.AddWithValue("@Username", taiKhoan.Username);
+					cmd.Parameters.AddWithValue("@Password", taiKhoan.Password);
+					cmd.Parameters.AddWithValue("@MaNV", taiKhoan.MaNV);
+					cmd.Parameters.AddWithValue("@CapDoQuyen", taiKhoan.CapDoQuyen);
+					cmd.Parameters.AddWithValue("@Avatar", taiKhoan.Avatar ?? (object)DBNull.Value);
+					cmd.Parameters.AddWithValue("@Disabled", taiKhoan.Disabled);
+
+					conn.Open();
+					int rowsAffected = cmd.ExecuteNonQuery();
+
+					if (rowsAffected == 0)
+					{
+						return false;
+					}
+					return true;
+				}
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public bool kiemTraTrungUsername(string username)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+			try
+			{
+				using (MySqlConnection conn = new MySqlConnection(connectionString))
+				{
+					string query = "SELECT COUNT(*) FROM TaiKhoan WHERE Username = @Username";
+					MySqlCommand cmd = new MySqlCommand(query, conn);
+					cmd.Parameters.AddWithValue("@Username", username);
+
+					conn.Open();
+					int count = Convert.ToInt32(cmd.ExecuteScalar());
+					return count > 0; // Trả về true nếu username đã tồn tại
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Lỗi: " + ex.Message);
+				return false;
+			}
+		}
+
+		public bool xoaTaiKhoan(TaiKhoan taiKhoan)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+			try
+			{
+				using (MySqlConnection conn = new MySqlConnection(connectionString))
+				{
+					string query = "UPDATE TaiKhoan SET Disabled = 1 WHERE Username = @Username";
+					MySqlCommand cmd = new MySqlCommand(query, conn);
+					cmd.Parameters.AddWithValue("@Username", taiKhoan.Username);
+					conn.Open();
+					cmd.ExecuteNonQuery();
+				}
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message); // Log error if needed
+				return false;
+			}
+		}
+
+		public bool hienThiLaiTaiKhoan(string username)
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+			try
+			{
+				using (MySqlConnection conn = new MySqlConnection(connectionString))
+				{
+					string query = "UPDATE TaiKhoan SET Disabled = 0 WHERE Username = @Username";
+					MySqlCommand cmd = new MySqlCommand(query, conn);
+					cmd.Parameters.AddWithValue("@Username", username);
+
+					conn.Open();
+					int rowsAffected = cmd.ExecuteNonQuery();
+
+					if (rowsAffected > 0)
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message); // Log lỗi nếu cần
+				return false;
+			}
+		}
+
 	}
 }
---
+++ UserControls/uc_QuanLyTaiKhoan.xaml.cs
@@ -0,0 +1,115 @@
+﻿using BUS;
+using DAL.DTO;
+using GUI.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI.UserControls
+{
+	/// <summary>
+	/// Interaction logic for uc_QuanLyTaiKhoan.xaml
+	/// </summary>
+	public partial class uc_QuanLyTaiKhoan : UserControl
+	{
+		ObservableCollection<TaiKhoan> list;
+		private CollectionView view;
+		
+		public uc_QuanLyTaiKhoan()
+		{
+			InitializeComponent();
+			TaiDanhSach();
+		}
+
+		private void TaiDanhSach()
+		{
+			list = new ObservableCollection<TaiKhoan>(TaiKhoanBUS.GetInstance().getDataTaiKhoan());
+			lsvTaiKhoan.ItemsSource = list;
+			view = (CollectionView)CollectionViewSource.GetDefaultView(list);
+			view.Filter = TaiKhoanFilter;
+		}
+
+		private bool TaiKhoanFilter(object obj)
+		{
+			if (String.IsNullOrEmpty(txtFilter.Text))
+				return true;
+			else
+				return (obj as TaiKhoan).Username.IndexOf(txtFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0;
+		}
+
+		void nhanData(TaiKhoan taiKhoan)
+		{
+			if (!TaiKhoanBUS.GetInstance().kiemTraTrungUsername(taiKhoan.Username))
+			{
+				if (TaiKhoanBUS.GetInstance().themTaiKhoan(taiKhoan))
+				{
+					new DialogCustoms("Thêm thành công", "Thông báo", DialogCustoms.OK).Show();
+					TaiDanhSach();
+				}
+			}
+			else
+			{
+				if (TaiKhoanBUS.GetInstance().hienThiLaiTaiKhoan(taiKhoan.Username))
+				{
+					new DialogCustoms("Tên dịch vụ đã tồn tại trong hệ thống\nĐã cập nhật lại danh sách", "Thông báo", DialogCustoms.OK).Show();
+					TaiDanhSach();
+				}
+			}
+		}
+
+		void capNhatData(TaiKhoan taiKhoan)
+		{
+			if (TaiKhoanBUS.GetInstance().capNhatTaiKhoan(taiKhoan))
+			{
+				new DialogCustoms("Cập nhật thành công", "Thông báo", DialogCustoms.OK).Show();
+				TaiDanhSach();
+			}
+		}
+
+		private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			CollectionViewSource.GetDefaultView(lsvTaiKhoan.ItemsSource).Refresh();
+		}
+
+		private void btnXoa_Click(object sender, RoutedEventArgs e)
+		{
+			TaiKhoan taiKhoan = (sender as Button).DataContext as TaiKhoan;
+
+			var thongbao = new DialogCustoms("Bạn có thật sự muốn xóa " + taiKhoan.Username, "Thông báo", DialogCustoms.YesNo);
+
+			if (thongbao.ShowDialog() == true)
+			{
+				new DialogCustoms("Xoá thành công", "Thông báo", DialogCustoms.OK).Show();
+				TaiKhoanBUS.GetInstance().xoaTaiKhoan(taiKhoan);
+				TaiDanhSach();
+			}
+		}
+
+		private void btnThem_Click(object sender, RoutedEventArgs e)
+		{
+			//Them_SuaTaiKhoan ThemTaiKhoan = new Them_SuaTaiKhoan(false);
+			//ThemTaiKhoan.truyen = new Them_SuaTaiKhoan.TryenDuLieu(nhanData);
+			//ThemTaiKhoan.ShowDialog();
+		}
+
+		private void btnCapNhat_Click(object sender, RoutedEventArgs e)
+		{
+			//TaiKhoan taiKhoan = (sender as Button).DataContext as TaiKhoan;
+			//Them_SuaTaiKhoan CapNhatTaiKhoan = new Them_SuaTaiKhoan(true, taiKhoan);
+			//CapNhatTaiKhoan.sua = new Them_SuaTaiKhoan.SuaDuLieu(capNhatData);
+			//Cap.ShowDialog();
+		}
+	}
+}
