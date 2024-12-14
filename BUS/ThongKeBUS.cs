a23938fff7bf55c9ac6eae068a12c91da2d4ae41
irisus1 <22520210@gm.uit.edu.vn>
2024-12-14 12:16:41 +0700

    add ThongKe

---
+++ BUS/ThongKeBUS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Data;
+using DAL;
+using DAL.DTO;
+
+
+namespace BUS
+{
+    public class ThongKeBUS
+    {
+        private static ThongKeBUS Instance;
+        private  ThongKeBUS()
+        {
+
+        }
+        public static ThongKeBUS GetInstance()
+        {
+            if (Instance == null) 
+            {
+                Instance = new ThongKeBUS();
+            }
+            return Instance;
+        }
+
+        public ThongKe LoadThongKeTheoThangNam(int month, int year)
+        {
+           return ThongKe_DAL.GetInstance().LoadThongKeTheoThangNam(month, year);
+        }
+        public List<CTHD> LoadThongKeTheoNam(int year)
+        {
+            return ThongKe_DAL.GetInstance().LoadThongKeTheoNam(year);
+        }
+    }
+  
+}
