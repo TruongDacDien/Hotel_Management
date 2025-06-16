b7326ac71cb3a79d5fab76a8285aad45fd54b04f
TruongDacDien <22520248@gm.uit.edu.vn>
2025-06-16 17:16:19 +0700

    Remove folder .vs

--- GUI/obj/Debug/net8.0-windows7.0/GUI_dji225zi_wpftmp.AssemblyInfo.cs
+++ GUI/obj/Debug/net8.0-windows7.0/GUI_dji225zi_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138b771cc5b1700554fd92c5e2dc3fb0a525f729")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49e48cfbecffebfb2766dca77eb9da5c59b4811")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
